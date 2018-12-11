d0b5aa75241a3b9dcf8da67a734adb766fbc55ef
ia-zenforce <45780389+ia-zenforce@users.noreply.github.com>
2018-12-11 13:11:30 +0800

    Add files via upload

---
+++ lambda.js
@@ -0,0 +1,46 @@
+var axios = require("axios");
+var async = require("async");
+var moment = require("moment");
+const server = "https://zenforce.zennerslab.com/api";
+const barcodeEntryUrl = `${server}/barcode_sync`;
+const manualEntryUrl = `${server}/manual_sync`;
+const returnsUrl = `${server}/returns_sync`;
+
+const date = moment()
+  .subtract(1, "days")
+  .format("YYYY-MM-DD");
+
+var params = { company_id: 144, sendTo: "ordiz663@gmail.com" };
+
+exports.handler = function(event, context, callback) {
+  context.callbackWaitsForEmptyEventLoop = false;
+  async.series(
+    [
+      function(cb) {
+        axios
+          .post(barcodeEntryUrl, params)
+          .then(res => cb(null, params))
+          .catch(err => cb(err));
+      },
+      function(cb) {
+        axios
+          .post(manualEntryUrl, params)
+          .then(res => cb(null, params))
+          .catch(err => cb(err));
+      },
+      function(cb) {
+        axios
+          .post(returnsUrl, params)
+          .then(res => cb(null, params))
+          .catch(err => cb(err));
+      }
+    ],
+    function(err, result) {
+      if (err) {
+        callback(err);
+      }
+
+      context.succeed(params);
+    }
+  );
+};
